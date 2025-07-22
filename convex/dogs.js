5d7d9c48c6c62f3acbdca79ad49fa5e57b3d6abb
Wei Ket <weiketong@getgo.sg>
2025-07-22 17:00:03 +0800

    Add dog

--- convex/dogs.js
+++ convex/dogs.js
@@ -1,16 +1,29 @@
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const list = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("dog").collect();
+    return await ctx.db.query("dogs").collect();
   },
 });
 
 export const get = query({
-  args: { id: v.id("dog") },
+  args: { id: v.id("dogs") },
   handler: async (ctx, args) => {
     return await ctx.db.get(args.id);
+  },
+});
+
+export const add = mutation({
+  args: { 
+    name: v.string(), 
+    gender: v.string()
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.insert("dogs", { 
+      name: args.name, 
+      gender: args.gender 
+    });
   },
 });
